74442877af7bed4f873f73f2fd2b2efe5fffffc8
Your Name <you@example.com>
2020-06-17 18:10:15 -0400

    Read more documentation, single line to tag two images

--- build.sh
+++ build.sh
@@ -1,3 +1,2 @@
 #!/usr/bin/env sh
-docker build . --file Dockerfile --tag s32ds-power-v1-2:$(git describe --long)
-docker build . --file Dockerfile --tag s32ds-power-v1-2:latest
+docker build . --file Dockerfile --tag s32ds-power-v1-2:latest --tag s32ds-power-v1-2:$(git describe --dirty --tags)
